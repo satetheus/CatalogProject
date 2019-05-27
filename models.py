39504db63513f4c502a57009689ab54cffee6bb5
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-05-27 01:35:55 -0600

    fixed foreignKey & added create_engine to models

--- models.py
+++ models.py
@@ -1,25 +1,32 @@
 #! /usr/bin/env python3
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(32), index=True)
-    
 
-class catagory(Base):
+
+class Catagory(Base):
     __tablename__ = 'catagory'
     id = Column(Integer, primary_key=True)
     name = Column(String(32), index=True)
-    
+
+
 class item(Base):
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
     name = Column(String(32), index=True)
-    owner_id = Column(Integer, ForeignKey(user.id), nullable=False)
-    catagory_id = = Column(Integer, ForeignKey(catagory.id), nullable=False)
+    owner_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    catagory_id = Column(Integer, ForeignKey(Catagory.id), nullable=False)
+
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
