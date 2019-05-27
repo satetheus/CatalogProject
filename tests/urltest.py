b4f8803805491e85582304f8ecc755c308bfaad5
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-05-26 22:47:59 -0600

    simplified url test code

--- tests/urltest.py
+++ tests/urltest.py
@@ -20,112 +20,39 @@ def testUrl(path, output):
     return False
 
 
-if testUrl('/catalog', "Homepage!"):
-    print("Test 1 success!")
-
-
-failcheck = False
-for owner_id in range(5):
-    path = '/catalog/owner/{}'.format(owner_id)
-    output = 'View items by owner {}.'.format(owner_id)
-    if not testUrl(path, output):
-        failcheck = True
-if not failcheck:
-    print("Test 2 success!")
-
-
-failcheck = False
-for owner_id in range(5):
-    for item_id in range(5):
-        path = '/catalog/owner/{}/item/{}'.format(owner_id, item_id)
-        output = 'This is item {} by owner {}.'.format(item_id, owner_id)
-        if not testUrl(path, output):
+def oneVar(path, output, testnum=1, loops=5):
+    failcheck = False
+    for var1 in range(loops):
+        testpath = path.format(var1)
+        testoutput = output.format(var1)
+        if not testUrl(testpath, testoutput):
             failcheck = True
-if not failcheck:
-    print("Test 3 success!")
+    if not failcheck:
+        print("Test {} success!".format(testnum))
 
 
-failcheck = False
-for owner_id in range(5):
-    path = '/catalog/owner/{}/item/new'.format(owner_id)
-    output = 'Make a new item by {}'.format(owner_id)
-    if not testUrl(path, output):
-        failcheck = True
-if not failcheck:
-    print("Test 4 success!")
+def twoVar(path, output, testnum=1, loops=5):
+    failcheck = False
+    for var1 in range(loops):
+        for var2 in range(loops):
+            # Output reverses var order for format of the phrases
+            testpath = path.format(var1, var2)
+            testoutput = output.format(var2, var1)
+            if not testUrl(testpath, testoutput):
+                failcheck = True
+    if not failcheck:
+        print("Test {} success!".format(testnum))
 
 
-failcheck = False
-for owner_id in range(5):
-    for item_id in range(5):
-        path = '/catalog/owner/{}/item/{}/edit'.format(owner_id, item_id)
-        output = 'Edit item {} by owner {}'.format(item_id, owner_id)
-        if not testUrl(path, output):
-            failcheck = True
-if not failcheck:
-    print("Test 5 success!")
-
-
-failcheck = False
-for owner_id in range(5):
-    for item_id in range(5):
-        path = '/catalog/owner/{}/item/{}/delete'.format(owner_id, item_id)
-        output = 'Delete item {} by owner {}'.format(item_id, owner_id)
-        if not testUrl(path, output):
-            failcheck = True
-if not failcheck:
-    print("Test 6 success!")
-
-
-failcheck = False
-for catagory_id in range(5):
-    path = '/catalog/catagory/{}'.format(catagory_id)
-    output = 'View items in catagory {}'.format(catagory_id)
-    if not testUrl(path, output):
-        failcheck = True
-if not failcheck:
-    print("Test 7 success!")
-
-
-failcheck = False
-for catagory_id in range(5):
-    for item_id in range(5):
-        path = '/catalog/catagory/{}/item/{}'.format(catagory_id, item_id)
-        output = 'This is item {} in catagory {}.'.format(item_id, catagory_id)
-        if not testUrl(path, output):
-            failcheck = True
-if not failcheck:
-    print("Test 8 success!")
-
-
-failcheck = False
-for catagory_id in range(5):
-    path = '/catalog/catagory/{}/item/new'.format(catagory_id)
-    output = 'Make a new item in catagory {}.'.format(catagory_id)
-    if not testUrl(path, output):
-        failcheck = True
-if not failcheck:
-    print("Test 9 success!")
-
-
-failcheck = False
-for catagory_id in range(5):
-    for item_id in range(5):
-        path = '/catalog/catagory/{}/item/{}/edit'.format(catagory_id, item_id)
-        output = 'Edit item {} in catagory {}.'.format(item_id, catagory_id)
-        if not testUrl(path, output):
-            failcheck = True
-if not failcheck:
-    print("Test 10 success!")
-
-
-failcheck = False
-for catagory_id in range(5):
-    for item_id in range(5):
-        path = '/catalog/catagory/{}/item/{}/delete'.format(
-            catagory_id, item_id)
-        output = 'Delete item {} in catagory {}.'.format(item_id, catagory_id)
-        if not testUrl(path, output):
-            failcheck = True
-if not failcheck:
-    print("Test 11 success!")
+# Run tests for given urls & expected outputs
+if testUrl('/catalog', "Homepage!"): print("Test 1 success!")
+oneVar('/catalog/owner/{}', 'View items by owner {}.', testnum=2)
+twoVar('/catalog/owner/{}/item/{}', 'This is item {} by owner {}.', testnum=3)
+oneVar('/catalog/owner/{}/item/new', 'Make a new item by {}', testnum=4)
+twoVar('/catalog/owner/{}/item/{}/edit', 'Edit item {} by owner {}', testnum=5)
+twoVar('/catalog/owner/{}/item/{}/delete', 'Delete item {} by owner {}', testnum=6)
+oneVar('/catalog/catagory/{}', 'View items in catagory {}', testnum=7)
+twoVar('/catalog/catagory/{}/item/{}', 'This is item {} in catagory {}.', testnum=8)
+oneVar('/catalog/catagory/{}/item/new', 'Make a new item in catagory {}.', testnum=9)
+twoVar('/catalog/catagory/{}/item/{}/edit', 'Edit item {} in catagory {}.', testnum=10)
+twoVar('/catalog/catagory/{}/item/{}/delete', 'Delete item {} in catagory {}.', testnum=11)
