99377b5d90a3d79acb7fff0eeac4fb557f590e36
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-06-07 16:44:42 -0600

    created apis for items

--- apis.py
+++ apis.py
@@ -28,25 +28,26 @@ api = Blueprint('api', __name__, template_folder='templates')
 #/api/all
 @api.route('/all')
 def api_showAll():
-  return 'all items'
+    items = session.query(Item).all()
+    return jsonify(allItems=[i.serialize for i in items])
 
 
 #/api/owner/<string:owner_name>
 @api.route('/owner/<string:owner_name>')
 def api_showOwner(owner_name):
-  return 'all items by {}'.format(owner_name)
+    items = session.query(Item).filter_by(owner=owner_name).all()
+    return jsonify(ownerItems=[i.serialize for i in items])
 
 
 #/api/catagory/<string:catagory_name>
-@api.route('/owner/<string:catagory_name>')
-def api_showOwner(catagory_name):
-  return 'all items in {}'.format(catagory_name)
-
+@api.route('/catagory/<string:catagory_name>')
+def api_showCatagory(catagory_name):
+    items = session.query(Item).filter_by(catagory=catagory_name).all()
+    return jsonify(catagoryItems=[i.serialize for i in items])
 
 
 #/api/item/<string:item_name>
 @api.route('/item/<string:item_name>')
-def api_showOwner(item_name):
-  return '{}'.format(item_name)
-
-
+def api_showItem(item_name):
+    item = session.query(Item).filter_by(name=item_name).first()
+    return jsonify(item.serialize)
