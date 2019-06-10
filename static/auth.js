0a57757151efe5c81810d89988f11cf9232afac2
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-06-10 10:04:16 -0600

    moved signInCallback to auth.js

--- static/auth.js
+++ static/auth.js
@@ -6,3 +6,30 @@ function start() {
         });
     });
 }
+
+function signInCallback(authResult) {
+    if (authResult['code']) {
+        $('#signinButton').attr('style', 'display: none');
+        $.ajax({
+            type: 'POST',
+            url: '/gconnect?state='+STATE,
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+            contentType: 'application/octet-stream; charset=utf-8',
+            success: function (result) {
+                $('#result').html('Login Successful!</br>' + result + '</br>Redirecting...')
+                setTimeout(function () {
+                    window.location.href = "/catalog/";
+                }, 2000);
+            },
+            processData: false,
+            data: authResult['code']
+        });
+    } else {
+        // handle error
+        console.log('There was an error: ' + authResult['error']);
+        $('#result').html('Failed to make a server-side call. Check your configuration and console.');
+
+    }
+}
