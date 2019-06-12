23bc16a7fd5cf201802a12bac62a926a9feccabb
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-06-11 21:07:43 -0600

    separated checkLogin & sqlItemSearch in controls

--- controls.py
+++ controls.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask import make_response, flash, Blueprint
 from flask import session as login_session
 
-#import oauth
+# import oauth
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 
@@ -25,40 +25,55 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-def checkLogin(local=False, checkby=None, checkVar=None):
-    """Checks that user is logged in. If local is set to true, 
-    this will check that the session username is the same as 
-    the owner of the db item.
+def checkLogin(local=False, crudItem=None):
+    """
+    If local is set to False (default), then checklogin checks of there is a
+    user signed in. If local is set to True, checklogin also checks if the
+    current user is the creator of the item 'crudItem'.
+
     Inputs:
       local: Boolean
-      checkby: String
-      checkVar: String
+      crudItem: SQLAlchemy query
     """
     if 'username' not in login_session:
         return False
 
     if local:
-        assert (checkby in [
-            'name', 'owner', 'catagory']), "checkby must be 'name', 'owner', or 'catagory'"
-        assert (checkVar != None), 'checkVar must not be None'
-        print(type(checkVar))
-        assert (type(checkVar) == str), 'checkVar must be a string'
-
-        search = session.query(Item)
-        if checkby.lower() == 'name':
-            crudItem = search.filter_by(name=checkVar).first()
-        elif checkby.lower() == 'owner':
-            crudItem = search.filter_by(owner=checkVar).all()
-        elif checkby.lower() == 'catagory':
-            crudItem = search.filter_by(catagory=checkVar).all()
-
         try:
             if crudItem.owner != login_session['username']:
                 return False
             else:
-                return crudItem
+                return True
         except AttributeError:
             if crudItem[0].owner != login_session['username']:
                 return False
             else:
-                return crudItem
+                return True
+    else:
+        return True
+
+
+def sqlItemSearch(checkby, checkVar):
+    """
+    Runs a search of the Item table, filtered by either 'name', 'owner', 
+    or 'catagory'.
+    Inputs:
+        checkby: String
+        checkVar: String
+    Outputs:
+        crudItem: SQLAlchemy query
+    """
+    assert type(checkby) == str, 'checkby must be a string'
+    assert checkby in [
+        'name', 'owner', 'catagory'], 'checkby can only be "name", "owner", or "catagory"'
+    assert type(checkVar) == str, 'checkVar must be a string'
+
+    search = session.query(Item)
+    if checkby.lower() == 'name':
+        crudItem = search.filter_by(name=checkVar).first()
+    elif checkby.lower() == 'owner':
+        crudItem = search.filter_by(owner=checkVar).all()
+    elif checkby.lower() == 'catagory':
+        crudItem = search.filter_by(catagory=checkVar).all()
+
+    return crudItem
