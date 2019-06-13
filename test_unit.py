511103bbe3e9a4da2d8a4d696177c644360be185
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-06-13 16:34:40 -0600

    corrected typo in test_unit

--- test_unit.py
+++ test_unit.py
@@ -13,5 +13,5 @@ def test_showAllItems():
     test1 = showAllItems()
     test2 = showAllItems(True)
 
-    assert type(test1) == sqlalchemy.orm.query.Query, 'showAllItems() is not returning a list.'
+    assert type(test1) == sqlalchemy.orm.query.Query, 'showAllItems() is not returning a query.'
     assert type(test2) == dict, 'showAllItems(True) is not returning a dict.'
