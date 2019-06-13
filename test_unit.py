b03f8d2917da462948d5ae276ea2ac7b4912e562
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-06-12 23:55:32 -0600

    added basic unit tests

---
+++ test_unit.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+from controls import sqlItemSearch, showAllItems
+import sqlalchemy
+
+
+def test_itemSearch():
+   assert sqlItemSearch('name', 'dopu').name == 'dopu', 'name didn\'t match or was absent.'
+   assert sqlItemSearch('owner', 'Vorian')[0].owner == 'Vorian', 'owner didn\'t match or was absent.'
+   assert sqlItemSearch('catagory', 'rpg')[0].catagory == 'rpg', 'catagory didn\'t match or was absent.'
+
+
+def test_showAllItems():
+    test1 = showAllItems()
+    test2 = showAllItems(True)
+
+    assert type(test1) == sqlalchemy.orm.query.Query, 'showAllItems() is not returning a list.'
+    assert type(test2) == dict, 'showAllItems(True) is not returning a dict.'
