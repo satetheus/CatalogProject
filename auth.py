fc88755cdaf30442410f1f1e0ff97144fd928f13
Satetheus <chriswarrenmcculloughwork@gmail.com>
2019-06-24 23:17:26 -0600

    removed unecessary prints from auth.py

--- auth.py
+++ auth.py
@@ -88,7 +88,6 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print("Token's client ID does not match app's.")
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -125,7 +124,6 @@ def gconnect():
     response = '<h1>Welcome, {}!</h1><img src="'.format(
         login_session['username'])
     flash("you are now logged in as %s" % login_session['username'])
-    print("done!")
     return response
 
 
